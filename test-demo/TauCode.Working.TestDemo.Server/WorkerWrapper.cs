95b27b20093329740c58e4086310e899745bf6df
Andrey Kovalenko <ak@taucode.com>
2020-08-25 20:02:22 +0300

    Missed file.

---
+++ test-demo/TauCode.Working.TestDemo.Server/WorkerWrapper.cs
@@ -0,0 +1,29 @@
+﻿using EasyNetQ;
+using System;
+using TauCode.Working.TestDemo.Common;
+
+namespace TauCode.Working.TestDemo.Server
+{
+    public class WorkerWrapper
+    {
+        private readonly IWorker _worker;
+        private readonly string _connectionString;
+
+        public WorkerWrapper(IWorker worker, string connectionString)
+        {
+            _worker = worker;
+            _connectionString = connectionString;
+        }
+
+        public void Run()
+        {
+            var bus = RabbitHutch.CreateBus(_connectionString);
+            bus.Respond<InvokeMethodRequest, InvokeMethodResponse>(this.ProcessMethodInvocation);
+        }
+
+        private InvokeMethodResponse ProcessMethodInvocation(InvokeMethodRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
