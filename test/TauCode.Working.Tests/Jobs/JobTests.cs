9d4dd2db51080086662407838ad1116fe87a04bd
Andrey Kovalenko <ak@taucode.com>
2020-09-19 22:28:44 +0300

    IJob.Parameter ut-s are ready.

--- test/TauCode.Working.Tests/Jobs/JobTests.cs
+++ test/TauCode.Working.Tests/Jobs/JobTests.cs
@@ -680,7 +680,7 @@ namespace TauCode.Working.Tests.Jobs
             var schedule1 = new SimpleSchedule(SimpleScheduleKind.Second, 1, start);
             var schedule2 = new SimpleSchedule(SimpleScheduleKind.Minute, 1, start);
             var schedule3 = new SimpleSchedule(SimpleScheduleKind.Hour, 1, start);
-            
+
 
             job.Schedule = schedule1;
             var readSchedule1 = job.Schedule;
@@ -1006,7 +1006,7 @@ namespace TauCode.Working.Tests.Jobs
             Assert.That(run1.Output, Is.EqualTo("Second Routine!"));
             Assert.That(output2, Is.EqualTo("First Routine!Second Routine!"));
         }
-        
+
         [Test]
         public void Routine_SetValidValueForEnabledOrDisabledJob_SetsValue()
         {
@@ -1157,9 +1157,9 @@ namespace TauCode.Working.Tests.Jobs
             Assert.That(run.Status, Is.EqualTo(JobRunStatus.Faulted));
             Assert.That(run.Exception, Is.SameAs(exception));
             Assert.That(run.Output, Does.Contain(exception.ToString()));
-            
+
             var log = _logWriter.ToString();
-            
+
             Assert.That(log, Does.Contain("Routine has thrown an exception."));
             Assert.That(log, Does.Contain($"Job 'my-job' completed synchronously. Reason of start was 'ScheduleDueTime'."));
 
@@ -1430,6 +1430,235 @@ namespace TauCode.Working.Tests.Jobs
 
         #endregion
 
+        #region IJob.Parameter
+
+        [Test]
+        public void Parameter_JustCreated_EqualsToNull()
+        {
+            // Arrange
+            var start = "2000-01-01Z".ToUtcDayOffset();
+            var timeMachine = ShiftedTimeProvider.CreateTimeMachine(start);
+            TimeProvider.Override(timeMachine);
+
+            using IJobManager jobManager = TestHelper.CreateJobManager();
+            jobManager.Start();
+            var job = jobManager.Create("my-job");
+
+            // Act
+            var parameter = job.Parameter;
+
+            // Assert
+            Assert.That(parameter, Is.Null);
+        }
+
+        [Test]
+        public void Parameter_ValueIsSet_EqualsToThatValue()
+        {
+            // Arrange
+            var start = "2000-01-01Z".ToUtcDayOffset();
+            var timeMachine = ShiftedTimeProvider.CreateTimeMachine(start);
+            TimeProvider.Override(timeMachine);
+
+            using IJobManager jobManager = TestHelper.CreateJobManager();
+            jobManager.Start();
+            var job = jobManager.Create("my-job");
+
+            // Act
+            object parameter1 = 1;
+            job.Parameter = parameter1;
+            var readParameter1 = job.Parameter;
+
+            job.IsEnabled = true;
+            object parameter2 = "hello";
+            job.Parameter = parameter2;
+            var readParameter2 = job.Parameter;
+
+            job.IsEnabled = false;
+            object parameter3 = null;
+            job.Parameter = parameter3;
+            var readParameter3 = job.Parameter;
+
+            // Assert
+            Assert.That(parameter1, Is.EqualTo(readParameter1));
+            Assert.That(parameter2, Is.EqualTo(readParameter2));
+            Assert.That(parameter3, Is.EqualTo(readParameter3));
+        }
+
+        /// <summary>
+        /// 0---------1---------2---------3---------4---------5---------
+        ///           |_____1.5s_____|    |_____1.5s_____|               
+        /// _____!1_________!2__________________________________________ (!1 - parameter1, !2 - parameter2)
+        /// </summary>
+        [Test]
+        public async Task Parameter_SetOnTheFly_RunsWithOldParameterAndNextTimeRunsWithNewParameter()
+        {
+            // Arrange
+            var start = "2000-01-01Z".ToUtcDayOffset();
+            var timeMachine = ShiftedTimeProvider.CreateTimeMachine(start);
+            TimeProvider.Override(timeMachine);
+
+            using IJobManager jobManager = TestHelper.CreateJobManager();
+            jobManager.Start();
+            var job = jobManager.Create("my-job");
+
+            ISchedule schedule = new ConcreteSchedule(
+                start.AddSeconds(1),
+                start.AddSeconds(3));
+
+            job.Schedule = schedule;
+            job.Output = new StringWriterWithEncoding(Encoding.UTF8);
+
+            object parameter1 = "Olia";
+            object parameter2 = "Ira";
+
+            job.Routine = async (parameter, tracker, writer, token) =>
+            {
+                await Task.Delay(1500, token);
+                await writer.WriteAsync($"Hello, {parameter}!");
+            };
+
+            job.IsEnabled = true;
+
+            // Act
+            await timeMachine.WaitUntilSecondsElapse(start, 0.8);
+            job.Parameter = parameter1;
+
+            await timeMachine.WaitUntilSecondsElapse(start, 1.3);
+            job.Parameter = parameter2;
+
+            await timeMachine.WaitUntilSecondsElapse(start, 2.8);
+            var output0 = job.Output.ToString();
+
+            await timeMachine.WaitUntilSecondsElapse(start, 4.8);
+            var output1 = job.Output.ToString();
+
+            var info = job.GetInfo(null);
+
+            // Assert
+            Assert.That(info.CurrentRun, Is.Null);
+            Assert.That(info.RunCount, Is.EqualTo(2));
+            Assert.That(info.Runs, Has.Count.EqualTo(2));
+
+            var run0 = info.Runs[0];
+            var run1 = info.Runs[1];
+
+            Assert.That(run0.Output, Is.EqualTo("Hello, Olia!"));
+            Assert.That(output0, Is.EqualTo("Hello, Olia!"));
+
+            Assert.That(run1.Output, Is.EqualTo("Hello, Ira!"));
+            Assert.That(output1, Is.EqualTo("Hello, Olia!Hello, Ira!"));
+        }
+
+        /// <summary>
+        /// 0---------1---------2---------3---------4---------5---------
+        ///           |_____1.5s_____|    |_____1.5s_____|               
+        /// _____!1_____________________!2______________________________ (!1 - parameter1, !2 - parameter2)
+        /// </summary>
+        [Test]
+        public async Task Parameter_SetAfterFirstRun_NextTimeRunsWithNewParameter()
+        {
+            // Arrange
+            var start = "2000-01-01Z".ToUtcDayOffset();
+            var timeMachine = ShiftedTimeProvider.CreateTimeMachine(start);
+            TimeProvider.Override(timeMachine);
+
+            using IJobManager jobManager = TestHelper.CreateJobManager();
+            jobManager.Start();
+            var job = jobManager.Create("my-job");
+
+            ISchedule schedule = new ConcreteSchedule(
+                start.AddSeconds(1),
+                start.AddSeconds(3));
+
+            job.Schedule = schedule;
+            job.Output = new StringWriterWithEncoding(Encoding.UTF8);
+
+            object parameter1 = "Olia";
+            object parameter2 = "Ira";
+
+            job.Routine = async (parameter, tracker, writer, token) =>
+            {
+                await Task.Delay(1500, token);
+                await writer.WriteAsync($"Hello, {parameter}!");
+            };
+
+            job.IsEnabled = true;
+
+            // Act
+            await timeMachine.WaitUntilSecondsElapse(start, 0.8);
+            job.Parameter = parameter1;
+
+            await timeMachine.WaitUntilSecondsElapse(start, 2.8);
+            var output0 = job.Output.ToString();
+
+            job.Parameter = parameter2;
+
+            await timeMachine.WaitUntilSecondsElapse(start, 4.8);
+            var output1 = job.Output.ToString();
+
+            var info = job.GetInfo(null);
+
+            // Assert
+            Assert.That(info.CurrentRun, Is.Null);
+            Assert.That(info.RunCount, Is.EqualTo(2));
+            Assert.That(info.Runs, Has.Count.EqualTo(2));
+
+            var run0 = info.Runs[0];
+            var run1 = info.Runs[1];
+
+            Assert.That(run0.Output, Is.EqualTo("Hello, Olia!"));
+            Assert.That(output0, Is.EqualTo("Hello, Olia!"));
+
+            Assert.That(run1.Output, Is.EqualTo("Hello, Ira!"));
+            Assert.That(output1, Is.EqualTo("Hello, Olia!Hello, Ira!"));
+        }
+
+        [Test]
+        public void Parameter_JobIsDisposed_CanBeRead()
+        {
+            // Arrange
+            var start = "2000-01-01Z".ToUtcDayOffset();
+            var timeMachine = ShiftedTimeProvider.CreateTimeMachine(start);
+            TimeProvider.Override(timeMachine);
+
+            using IJobManager jobManager = TestHelper.CreateJobManager();
+            jobManager.Start();
+            var job = jobManager.Create("my-job");
+
+            // Act
+            job.Parameter = 17;
+            job.Dispose();
+
+
+            // Assert
+            Assert.That(job.Parameter, Is.EqualTo(17));
+        }
+
+        [Test]
+        public void Parameter_JobIsDisposedThenValueIsSet_ThrowsJobObjectDisposedException()
+        {
+            // Arrange
+            var start = "2000-01-01Z".ToUtcDayOffset();
+            var timeMachine = ShiftedTimeProvider.CreateTimeMachine(start);
+            TimeProvider.Override(timeMachine);
+
+            using IJobManager jobManager = TestHelper.CreateJobManager();
+            jobManager.Start();
+            var job = jobManager.Create("my-job");
+
+            // Act
+            job.Parameter = 17;
+            job.Dispose();
+            var ex = Assert.Throws<JobObjectDisposedException>(() => job.Parameter = 101);
+            
+            // Assert
+            Assert.That(job.Parameter, Is.EqualTo(17));
+            Assert.That(ex.ObjectName, Is.EqualTo("my-job"));
+        }
+
+        #endregion
+
+
         //====================================================================================
 
         [Test]
@@ -1524,19 +1753,9 @@ namespace TauCode.Working.Tests.Jobs
 
 
 
-
-
-        // todo: IJob.Parameter
-        // - initially, null
-        // - can be set to any value including null (if not running), be it enabled or disabled.
-        // - if set while running, finishes job with current parameter; next run is performed with new parameter.
-        // - when set after run completed, afterwards runs with new parameter value.
-        // - after disposed, can be read.
-        // - after disposed, cannot be set, throws.
-
         // todo: IJob.ProgressTracker
         // - initially, null
-        // - can be set to any value including null (if not running), be it enabled or disabled.
+        // - can be set to any value including null, be it enabled or disabled.
         // - if set while running, finishes job with current progress tracker; next run is performed with new progress tracker.
         // - when set after run completed, afterwards runs with new progress tracker.
         // - after disposed, can be read.
@@ -1544,7 +1763,7 @@ namespace TauCode.Working.Tests.Jobs
 
         // todo: IJob.Output
         // - initially, null
-        // - can be set to any value including null (if not running), be it enabled or disabled.
+        // - can be set to any value including null, be it enabled or disabled.
         // - if set while running, finishes job with current output; next run is performed with new output.
         // - when set after run completed, afterwards runs with new output.
         // - after disposed, can be read.
@@ -1580,6 +1799,12 @@ namespace TauCode.Working.Tests.Jobs
         // - was running => cancelled, returns true + logs
         // - was stopped => returns false
         // - disposed => throws
+
+        // todo: IJob.Wait(int)
+        // - ...
+
+        // todo: IJob.Wait(TimeSpan)
+        // - ...
 
         // todo: IJob.IsDisposed
         // - obvious
