a181d6ad91fb99197e4d6e73a82b09c88e8b56e6
Andrii Kovalenko <ankovalenko@deloitte.com>
2021-03-17 17:15:15 +0200

    Started to add laborer tests.

---
+++ test/TauCode.Working.Tests/Labor/DemoLaborer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TauCode.Working.Labor;
+
+namespace TauCode.Working.Tests.Labor
+{
+    public class DemoLaborer : LaborerBase
+    {
+        public override bool IsPausingSupported => true;
+
+        protected override void OnStarting()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStarted()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStopping()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStopped()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnPausing()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnPaused()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnResuming()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnResumed()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnDisposed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/TauCode.Working.Tests/Labor/LaborerTests.cs
@@ -0,0 +1,124 @@
+﻿using NUnit.Framework;
+using TauCode.Working.Labor;
+
+namespace TauCode.Working.Tests.Labor
+{
+    [TestFixture]
+    public class LaborerTests
+    {
+        #region Constructor
+
+        // todo: happy path on ctor
+
+        [Test]
+        public void Constructor_NoArguments_RunsOk()
+        {
+            // Arrange
+
+            // Act
+            ILaborer laborer = new DemoLaborer();
+ 
+            // Assert
+            Assert.That(laborer.Name, Is.Null);
+            Assert.That(laborer.State, Is.EqualTo(LaborerState.Stopped));
+            Assert.That(laborer.IsDisposed, Is.False);
+            Assert.That(laborer.Logger, Is.Null);
+        }
+
+        #endregion
+
+        #region Name
+
+        // todo: changed to non-null => Ok
+        // todo: changed to null => ok
+        // todo: <disposed> => name not changed, can read, not write
+
+        #endregion
+
+        #region Start
+
+        // todo: Stopped => ok
+        // todo: Starting => waits, ex
+        // todo: Running => ex
+        // todo: Stopping => waits, ok
+        // todo: Pausing => waits, ex
+        // todo: Paused => ex
+        // todo: Resuming => waits, ex
+        // todo: Start, stop, start => ok
+        // todo: <disposed> => ex
+
+        #endregion
+
+        #region Stop
+
+        // todo: Stopped => ex
+        // todo: Starting => waits, ok
+        // todo: Running => ok
+        // todo: Stopping => waits, ex
+        // todo: Pausing => waits, ok
+        // todo: Paused => ok
+        // todo: Resuming => waits, ex
+        // todo: Start, stop, start, stop => ok
+        // todo: <disposed> => ex
+
+        #endregion
+
+        #region Pause
+
+        // todo: Stopped => ex
+        // todo: Starting => waits, ok
+        // todo: Running => ok
+        // todo: Stopping => waits, ex
+        // todo: Pausing => waits, ex
+        // todo: Paused => ex
+        // todo: Resuming => waits, ok
+        // todo: Start, pause, resume, pause => ok
+        // todo: <disposed> => ex
+
+        #endregion
+
+        #region Resume
+
+        // todo: Stopped => ex
+        // todo: Starting => waits, ex
+        // todo: Running => ex
+        // todo: Stopping => waits, ex
+        // todo: Pausing => waits, ok
+        // todo: Paused => ok
+        // todo: Resuming => waits, ex
+        // todo: Start, pause, resume, pause, resume => ok
+        // todo: <disposed> => ex
+
+        #endregion
+
+        #region Dispose
+
+        // todo: Stopped => ok
+        // todo: Starting => waits, ok
+        // todo: Running => ok
+        // todo: Stopping => waits, ok
+        // todo: Pausing => waits, ok
+        // todo: Paused => ok
+        // todo: Resuming => waits, ok
+        // todo: Start, pause, resume, pause, resume => ok
+        // todo: <disposed> => does nothing, ok
+
+        #endregion
+
+        #region Name
+
+        // todo: changed to non-null => ok
+        // todo: changed to null => ok
+        // todo: <disposed> => can get, can't set
+
+        #endregion
+
+        #region ILogger
+
+        // todo: changed to non-null => ok
+        // todo: changed to null => ok
+        // todo: <disposed> => can get, can't set
+
+        #endregion
+    }
+}
