74916f0db0a546c36cff668efa531886204e376e
Andrey Kovalenko <ak@taucode.com>
2022-08-08 03:58:01 +0300

    Updated content for 1.5.0.

--- src/TauCode.Working/WorkerBase.cs
+++ src/TauCode.Working/WorkerBase.cs
@@ -1,316 +1,313 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using System;
 using System.Text;
-using System.Threading;
 
-namespace TauCode.Working
+namespace TauCode.Working;
+
+public abstract class WorkerBase : IWorker
 {
-    public abstract class WorkerBase : IWorker
+    #region Fields
+
+    private long _stateValue;
+    private long _isDisposedValue;
+
+    private string _name;
+    private ILogger _logger;
+
+    private readonly object _dataLock;
+
+    private readonly object _controlLock;
+
+    #endregion
+
+    #region Constructor
+
+    protected WorkerBase()
     {
-        #region Fields
+        _dataLock = new object();
+        _controlLock = new object();
 
-        private long _stateValue;
-        private long _isDisposedValue;
+        this.SetState(WorkerState.Stopped);
+        this.SetIsDisposed(false);
+    }
 
-        private string _name;
-        private ILogger _logger;
+    #endregion
 
-        private readonly object _dataLock;
+    #region Private
 
-        private readonly object _controlLock;
+    private WorkerState GetState()
+    {
+        var stateValue = Interlocked.Read(ref _stateValue);
+        return (WorkerState)stateValue;
+    }
 
-        #endregion
+    private void SetState(WorkerState state)
+    {
+        var stateValue = (long)state;
+        Interlocked.Exchange(ref _stateValue, stateValue);
+    }
 
-        #region Constructor
+    private bool GetIsDisposed()
+    {
+        var isDisposedValue = Interlocked.Read(ref _isDisposedValue);
+        return isDisposedValue == 1L;
+    }
 
-        protected WorkerBase()
+    private void SetIsDisposed(bool isDisposed)
+    {
+        var isDisposedValue = isDisposed ? 1L : 0L;
+        Interlocked.Exchange(ref _isDisposedValue, isDisposedValue);
+    }
+
+    private NotSupportedException CreatePausingNotSupportedException()
+    {
+        return new NotSupportedException("Pausing/resuming is not supported.");
+    }
+
+    #endregion
+
+    #region Abstract
+
+    public abstract bool IsPausingSupported { get; }
+
+    protected abstract void OnStarting();
+    protected abstract void OnStarted();
+
+    protected abstract void OnStopping();
+    protected abstract void OnStopped();
+
+    protected abstract void OnPausing();
+    protected abstract void OnPaused();
+
+    protected abstract void OnResuming();
+    protected abstract void OnResumed();
+
+    protected abstract void OnDisposed();
+
+    #endregion
+
+    #region Protected
+
+    protected ILogger GetSafeLogger() => this.Logger ?? NullLogger.Instance;
+
+    protected void Stop(bool throwOnDisposedOrWrongState)
+    {
+        lock (_controlLock)
         {
-            _dataLock = new object();
-            _controlLock = new object();
+            if (this.GetIsDisposed())
+            {
+                if (throwOnDisposedOrWrongState)
+                {
+                    throw new ObjectDisposedException(this.Name);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            var state = this.GetState();
+            var isValidState =
+                state == WorkerState.Running ||
+                state == WorkerState.Paused;
+
+            if (!isValidState)
+            {
+                if (throwOnDisposedOrWrongState)
+                {
+                    throw this.CreateInvalidOperationException(nameof(Stop), state);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            this.SetState(WorkerState.Stopping);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is stopping.");
+            this.OnStopping();
 
             this.SetState(WorkerState.Stopped);
-            this.SetIsDisposed(false);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is stopped.");
+            this.OnStopped();
         }
-
-        #endregion
-
-        #region Private
-
-        private WorkerState GetState()
-        {
-            var stateValue = Interlocked.Read(ref _stateValue);
-            return (WorkerState)stateValue;
-        }
-
-        private void SetState(WorkerState state)
-        {
-            var stateValue = (long)state;
-            Interlocked.Exchange(ref _stateValue, stateValue);
-        }
-
-        private bool GetIsDisposed()
-        {
-            var isDisposedValue = Interlocked.Read(ref _isDisposedValue);
-            return isDisposedValue == 1L;
-        }
-
-        private void SetIsDisposed(bool isDisposed)
-        {
-            var isDisposedValue = isDisposed ? 1L : 0L;
-            Interlocked.Exchange(ref _isDisposedValue, isDisposedValue);
-        }
-
-        private NotSupportedException CreatePausingNotSupportedException()
-        {
-            return new NotSupportedException("Pausing/resuming is not supported.");
-        }
-
-        #endregion
-
-        #region Abstract
-
-        public abstract bool IsPausingSupported { get; }
-
-        protected abstract void OnStarting();
-        protected abstract void OnStarted();
-
-        protected abstract void OnStopping();
-        protected abstract void OnStopped();
-
-        protected abstract void OnPausing();
-        protected abstract void OnPaused();
-
-        protected abstract void OnResuming();
-        protected abstract void OnResumed();
-
-        protected abstract void OnDisposed();
-
-        #endregion
-
-        #region Protected
-
-        protected ILogger GetSafeLogger() => this.Logger ?? NullLogger.Instance;
-
-        protected void Stop(bool throwOnDisposedOrWrongState)
-        {
-            lock (_controlLock)
-            {
-                if (this.GetIsDisposed())
-                {
-                    if (throwOnDisposedOrWrongState)
-                    {
-                        throw new ObjectDisposedException(this.Name);
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-
-                var state = this.GetState();
-                var isValidState =
-                    state == WorkerState.Running ||
-                    state == WorkerState.Paused;
-
-                if (!isValidState)
-                {
-                    if (throwOnDisposedOrWrongState)
-                    {
-                        throw this.CreateInvalidOperationException(nameof(Stop), state);
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-
-                this.SetState(WorkerState.Stopping);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is stopping.");
-                this.OnStopping();
-
-                this.SetState(WorkerState.Stopped);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is stopped.");
-                this.OnStopped();
-            }
-        }
-        
-        protected InvalidOperationException CreateInvalidOperationException(string requestedOperation, WorkerState state)
-        {
-            var sb = new StringBuilder();
-            sb.Append($"Cannot perform operation '{requestedOperation}'. Worker state is '{state}'.");
-            if (this.Name != null)
-            {
-                sb.Append($" Worker name is '{this.Name}'.");
-            }
-
-            var message = sb.ToString();
-
-            return new InvalidOperationException(message);
-        }
-
-        #endregion
-
-        #region IWorker Members
-
-        public string Name
-        {
-            get
-            {
-                lock (_dataLock)
-                {
-                    return _name;
-                }
-            }
-            set
-            {
-                if (this.GetIsDisposed())
-                {
-                    throw new ObjectDisposedException(this.Name);
-                }
-
-                lock (_dataLock)
-                {
-                    _name = value;
-                }
-            }
-        }
-
-        public WorkerState State => this.GetState();
-
-        public void Start()
-        {
-            lock (_controlLock)
-            {
-                if (this.GetIsDisposed())
-                {
-                    throw new ObjectDisposedException(this.Name);
-                }
-
-                var state = this.GetState();
-                if (state != WorkerState.Stopped)
-                {
-                    throw this.CreateInvalidOperationException(nameof(Start), state);
-
-                }
-
-                this.SetState(WorkerState.Starting);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is starting.");
-                this.OnStarting(); // todo: if thrown, worker remains in 'Starting' state! critical error!
-
-                this.SetState(WorkerState.Running);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is started.");
-                this.OnStarted();
-            }
-        }
-
-        public void Stop() => this.Stop(true);
-
-        public void Pause()
-        {
-            if (!IsPausingSupported)
-            {
-                throw this.CreatePausingNotSupportedException();
-            }
-
-            lock (_controlLock)
-            {
-                if (this.GetIsDisposed())
-                {
-                    throw new ObjectDisposedException(this.Name);
-                }
-
-                var state = this.GetState();
-                if (state != WorkerState.Running)
-                {
-                    throw this.CreateInvalidOperationException(nameof(Pause), state);
-                }
-
-                this.SetState(WorkerState.Pausing);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is pausing.");
-                this.OnPausing();
-
-                this.SetState(WorkerState.Paused);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is paused.");
-                this.OnPaused();
-            }
-        }
-
-        public void Resume()
-        {
-            if (!IsPausingSupported)
-            {
-                throw this.CreatePausingNotSupportedException();
-            }
-
-            lock (_controlLock)
-            {
-                if (this.GetIsDisposed())
-                {
-                    throw new ObjectDisposedException(this.Name);
-                }
-
-                var state = this.GetState();
-                if (state != WorkerState.Paused)
-                {
-                    throw this.CreateInvalidOperationException(nameof(Resume), state);
-                }
-
-                this.SetState(WorkerState.Resuming);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is resuming.");
-                this.OnResuming();
-
-                this.SetState(WorkerState.Running);
-                this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is resumed.");
-                this.OnResumed();
-            }
-        }
-
-        public bool IsDisposed => this.GetIsDisposed();
-        public ILogger Logger
-        {
-            get
-            {
-                lock (_dataLock)
-                {
-                    return _logger;
-                }
-            }
-            set
-            {
-                if (this.GetIsDisposed())
-                {
-                    throw new ObjectDisposedException(this.Name);
-                }
-
-                lock (_dataLock)
-                {
-                    _logger = value;
-                }
-            }
-        }
-
-        #endregion
-
-        #region IDisposable Members
-
-        public void Dispose()
-        {
-            lock (_controlLock)
-            {
-                if (this.GetIsDisposed())
-                {
-                    return; // won't dispose twice
-                }
-
-                this.Stop(false);
-
-                this.SetIsDisposed(true);
-
-                this.OnDisposed();
-            }
-        }
-
-        #endregion
     }
+
+    protected InvalidOperationException CreateInvalidOperationException(string requestedOperation, WorkerState state)
+    {
+        var sb = new StringBuilder();
+        sb.Append($"Cannot perform operation '{requestedOperation}'. Worker state is '{state}'.");
+        if (this.Name != null)
+        {
+            sb.Append($" Worker name is '{this.Name}'.");
+        }
+
+        var message = sb.ToString();
+
+        return new InvalidOperationException(message);
+    }
+
+    #endregion
+
+    #region IWorker Members
+
+    public string Name
+    {
+        get
+        {
+            lock (_dataLock)
+            {
+                return _name;
+            }
+        }
+        set
+        {
+            if (this.GetIsDisposed())
+            {
+                throw new ObjectDisposedException(this.Name);
+            }
+
+            lock (_dataLock)
+            {
+                _name = value;
+            }
+        }
+    }
+
+    public WorkerState State => this.GetState();
+
+    public void Start()
+    {
+        lock (_controlLock)
+        {
+            if (this.GetIsDisposed())
+            {
+                throw new ObjectDisposedException(this.Name);
+            }
+
+            var state = this.GetState();
+            if (state != WorkerState.Stopped)
+            {
+                throw this.CreateInvalidOperationException(nameof(Start), state);
+
+            }
+
+            this.SetState(WorkerState.Starting);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is starting.");
+            this.OnStarting(); // todo: if thrown, worker remains in 'Starting' state! critical error!
+
+            this.SetState(WorkerState.Running);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is started.");
+            this.OnStarted();
+        }
+    }
+
+    public void Stop() => this.Stop(true);
+
+    public void Pause()
+    {
+        if (!IsPausingSupported)
+        {
+            throw this.CreatePausingNotSupportedException();
+        }
+
+        lock (_controlLock)
+        {
+            if (this.GetIsDisposed())
+            {
+                throw new ObjectDisposedException(this.Name);
+            }
+
+            var state = this.GetState();
+            if (state != WorkerState.Running)
+            {
+                throw this.CreateInvalidOperationException(nameof(Pause), state);
+            }
+
+            this.SetState(WorkerState.Pausing);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is pausing.");
+            this.OnPausing();
+
+            this.SetState(WorkerState.Paused);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is paused.");
+            this.OnPaused();
+        }
+    }
+
+    public void Resume()
+    {
+        if (!IsPausingSupported)
+        {
+            throw this.CreatePausingNotSupportedException();
+        }
+
+        lock (_controlLock)
+        {
+            if (this.GetIsDisposed())
+            {
+                throw new ObjectDisposedException(this.Name);
+            }
+
+            var state = this.GetState();
+            if (state != WorkerState.Paused)
+            {
+                throw this.CreateInvalidOperationException(nameof(Resume), state);
+            }
+
+            this.SetState(WorkerState.Resuming);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is resuming.");
+            this.OnResuming();
+
+            this.SetState(WorkerState.Running);
+            this.GetSafeLogger().LogDebug($"Worker '{this.Name}' is resumed.");
+            this.OnResumed();
+        }
+    }
+
+    public bool IsDisposed => this.GetIsDisposed();
+    public ILogger Logger
+    {
+        get
+        {
+            lock (_dataLock)
+            {
+                return _logger;
+            }
+        }
+        set
+        {
+            if (this.GetIsDisposed())
+            {
+                throw new ObjectDisposedException(this.Name);
+            }
+
+            lock (_dataLock)
+            {
+                _logger = value;
+            }
+        }
+    }
+
+    #endregion
+
+    #region IDisposable Members
+
+    public void Dispose()
+    {
+        lock (_controlLock)
+        {
+            if (this.GetIsDisposed())
+            {
+                return; // won't dispose twice
+            }
+
+            this.Stop(false);
+
+            this.SetIsDisposed(true);
+
+            this.OnDisposed();
+        }
+    }
+
+    #endregion
 }
--- src/TauCode.Working/WorkerState.cs
+++ src/TauCode.Working/WorkerState.cs
@@ -1,13 +1,12 @@
-﻿namespace TauCode.Working
+﻿namespace TauCode.Working;
+
+public enum WorkerState
 {
-    public enum WorkerState
-    {
-        Stopped = 1,
-        Starting = 2,
-        Running = 3,
-        Stopping = 4,
-        Pausing = 5,
-        Paused = 6,
-        Resuming = 7,
-    }
+    Stopped = 1,
+    Starting = 2,
+    Running = 3,
+    Stopping = 4,
+    Pausing = 5,
+    Paused = 6,
+    Resuming = 7,
 }
