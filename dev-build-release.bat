fdaded94b2eb0eaa3bb3dea8ee29fb49f1b7e56d
Andrey Kovalenko <ak@taucode.com>
2022-08-08 04:01:12 +0300

    Release 1.5.0.

--- dev-build-release.bat
+++
@@ -1,9 +0,0 @@
-dotnet restore
-
-dotnet build --configuration Debug
-dotnet build --configuration Release
-
-dotnet test -c Debug .\tests\TauCode.Working.Tests\TauCode.Working.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Working.Tests\TauCode.Working.Tests.csproj
-
-nuget pack nuget\TauCode.Working.nuspec
